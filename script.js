4213b29d07e5993548f368c73d7e112e5cbd08e9
Gavin Davenport <gavinkdavenport@gmail.com>
2024-05-31 19:51:53 -0700

    Work on has and remove functions
    has should be ready, remove needs work on linked list conditionals

--- script.js
+++ script.js
@@ -53,7 +53,11 @@ const makeHashMap = () => {
                     return null;
                 };
             } else {
-                return buckets[hashCode].value;
+                if (buckets[hashCode].key === key) {
+                    return buckets[hashCode].value;
+                } else {
+                    return null;
+                }
             }
         } else{
             // If a key is not found, return null
@@ -63,11 +67,63 @@ const makeHashMap = () => {
 
     const has = (key) => {
         // takes a key as an argument and returns true or false based on whether or not the key is in the hash map
+        let hashCode = hash(key);
+        if (buckets[hashCode]) {
+            if (buckets[hashCode].nextNode) {
+                let pointer = buckets[hashCode];
+                while (pointer.key !== key && pointer.nextNode !== null) {
+                    pointer = pointer.nextNode;
+                };
+                if (pointer.key === key) {
+                    return true;
+                } else {
+                    // If a key is not found, return false
+                    return false;
+                };
+            } else {
+                if (buckets[hashCode].key === key) {
+                    return true;
+                } else {
+                    return false;
+                };
+            }
+        } else{
+            // If a key is not found, return false
+            return false;
+        };
     };
 
     const remove = (key) => {
         // If the given key is in the hash map, it should remove the entry with that key and return true
         // If the key isn’t in the hash map, it should return false
+        let hashCode = hash(key);
+        if (buckets[hashCode]) {
+            if (buckets[hashCode].nextNode) {
+                let pointer = buckets[hashCode];
+                while (pointer.key !== key && pointer.nextNode !== null) {
+                    pointer = pointer.nextNode;
+                };
+                if (pointer.key === key) {
+                    //remove node from linked list in the place the pointer lands
+                    //DO THINGS HERE
+                    return true;
+                } else {
+                    // If a key is not found, return false
+                    return false;
+                };
+            } else {
+                if (buckets[hashCode].key === key) {
+                    buckets.splice(hashCode, 1, null)
+                    return true;
+                } else {
+                    return false;
+                };
+            }
+        } else{
+            // If a key is not found, return false
+            return false;
+        };
+    };
     };
 
     const length = () => {
